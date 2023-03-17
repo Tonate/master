0537f47b2230af3df42df7bb9869171081776627
Donggoo Kim <donggookim@Donggoos-MacBook-Air.local>
2023-03-18 08:57:11 +0900

    Fix: Final touch

---
+++ src/contract_FunC/build.sh
@@ -0,0 +1,4 @@
+npx func-js stdlib.fc tonate_public_random.fc --boc ./build_artifacts/tonate_public_random.cell
+npx func-js stdlib.fc tonate_public_split.fc --boc ./build_artifacts/tonate_public_split.cell
+npx func-js stdlib.fc tonate_private_random.fc --boc ./build_artifacts/tonate_private_random.cell
+npx func-js stdlib.fc tonate_private_split.fc --boc ./build_artifacts/tonate_private_split.cell
